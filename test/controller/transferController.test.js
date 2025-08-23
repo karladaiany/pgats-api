f087c9a4da4a293c4561953d09b18ec34eb9fd21
Karla Daiany <karllinhadeoliveira@live.com>
2025-08-22 21:24:45 -0300

    Adicionando testes controller

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -6,9 +6,12 @@ const { expect } = require('chai');
 //Aplicação
 const app = require('../../app');
 
+// Mock
+const transferService = require('../../service/transferService');
+
 // Testes
 describe('Transfer Controller', () => {
-    describe('POST /transfers', () => {
+    describe('POST /transfer', () => {
         it('Quando informo remetente e destinatario inexistentes recebo 400', async () => {
             const resposta = await request(app)
                 .post('/transfer')
@@ -21,6 +24,53 @@ describe('Transfer Controller', () => {
             expect(resposta.status).to.equal(400)
             expect(resposta.body).to.have.property('error', 'Usuário não encontrado')
         })
+
+        it('Usando Mocks: Quando informo remetente e destinatário inexistentes recebo 400', async () => {
+            // Mocar apenas a função transferValue
+            const transferServiceMock = sinon.stub(transferService, 'transferValue');
+            transferServiceMock.throws(new Error('Usuário não encontrado'));
+
+            const resposta = await request(app)
+                .post('/transfer')
+                .send({
+                    from: 'Karla',
+                    to: 'Samuel',
+                    amount: 100
+                })
+            
+            expect(resposta.status).to.equal(400)
+            expect(resposta.body).to.have.property('error', 'Usuário não encontrado')
+
+            // Reseto o Mock
+            sinon.restore()
+        })
+
+        it('Usando Mocks: Quando informo valores válidos eu tenho sucesso com 201 CREATED', async () => {
+            // Mocar apenas a função transferValue
+            const transferServiceMock = sinon.stub(transferService, 'transferValue');
+            transferServiceMock.returns({
+                from: 'Karla',
+                to: 'Samuel',
+                amount: 100,
+                date: new Date().toISOString()
+            });
+
+            const resposta = await request(app)
+                .post('/transfer')
+                .send({
+                    from: 'Karla',
+                    to: 'Samuel',
+                    amount: 100
+                })
+
+            expect(resposta.status).to.equal(201)
+            expect(resposta.body).to.have.property('from', 'Karla')
+            expect(resposta.body).to.have.property('to', 'Samuel')
+            expect(resposta.body).to.have.property('amount', 100)
+
+            // Reseto o Mock
+            sinon.restore()
+            })
     })
 
     describe('GET /transfers', () => {
